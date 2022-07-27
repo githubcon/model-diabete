e55ea6094043ee46858c3e648fd67c5c2421ed2d
mohamed <habiblearning0898@gmail.cvom>
2022-07-27 14:54:28 +0200

    add arriere plan

--- étude_diabète/app_exemple.py
+++ étude_diabète/app_exemple.py
@@ -3,10 +3,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
 import base64
-
 ####Code pour mettre un fond d'ecran
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
@@ -27,45 +27,33 @@ def set_background(png_file):
 
 set_background('data_scientist.png')
 
+
 # on crée le titre
-st.title('Welcome to Diabetes Prediction Application using Machine Learning Algorithms') 
-
-
-nom_fichier = ["RandomForest", "LogisticRegression", "DecisionTrees"] 
+st.title("Bienvenu sur l'application de prédiction de diabète grâce aux algorithmes de Machine Learning") 
+nom_fichier = ["KNN","LR","RandomForest"]
 
 ### Creation d'un volet contenant les 3 modèles
 
 with st.sidebar:
-    selected = option_menu("Main Menu", nom_fichier,
-		styles={
-        "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "green"},
-    })
-
-############ Ajout des images
-
-
-####selection des fichiers
+    selected = option_menu("Main Menu", nom_fichier)
 
 if selected == nom_fichier[0]:
-    st.title(f"select {selected}")
+    st.title(f"You selected {selected}")
 if selected == nom_fichier[1]:
-    st.title(f"select {selected}") 
+    st.title(f"You selected {selected}") 
 if selected == nom_fichier[2]:
-    st.title(f"select {selected}")
+    st.title(f"selected {selected}")
 
 
-file1 = open('diabetes_prediction_forest.pkl', 'rb')
+file1 = open('diabetes_prediction.pkl', 'rb')
 rf = pickle.load(file1)
 file1.close()
 
-file2= open('diabetes_prediction_logreg.pkl', 'rb')
+file2= open('diabetes_prediction_LR.pkl', 'rb')
 LR = pickle.load(file2)
 file2.close()
 
-file3 = open('diabetes_prediction_tree.pkl', 'rb')
+file3 = open('diabetes_prediction_forest.pkl', 'rb')
 forest = pickle.load(file3)
 file3.close()
 
@@ -74,8 +62,8 @@ data = pd.read_csv("diabete_population.csv")
 print(data)
   
  ### Recuperation des features :
-age = st.number_input("Enter your age") 
-grossesses = st.number_input("Enter your grossesses") 
+age = st.slider("Select your age", 0, 100) 
+grossesses = st.slider("Select your grossesses", 0, 10) 
 insuline= st.number_input("Enter your insuline") 
 
 # ##### Normalisation des features################################
@@ -102,17 +90,21 @@ if(st.button('Predict Diabete')):
         query = query.reshape(1, 3)
         print(query)
         prediction = rf.predict(query)[0]
-        st.title("Predicted value " +
-                 str(prediction) + str(nom_fichier[0]))
-    
+        if prediction == 0:
+            st.title("Valeur prédite : " + str(prediction) + " Vous êtes probablement non diabétique ! modèle utilisé :" + str(nom_fichier[0]))
+        else:
+            st.title("Valeur prédite : " + str(prediction) + " Vous êtes probablement diabétique !  modèle utilisé :" + str(nom_fichier[0]))
+
     elif(selected == nom_fichier[1]):
         query = np.array([grossesses, age, insuline])
 
         query = query.reshape(1, 3)
         print(query)
         prediction = LR.predict(query)[0]
-        st.title("Predicted value " +
-                 str(prediction) + str(nom_fichier[1]))
+        if prediction == 0:
+            st.title("Valeur prédite : " + str(prediction) + " Vous êtes probablement non diabétique ! modèle utilisé : " + str(nom_fichier[1]))
+        else:
+            st.title("Valeur prédite : " + str(prediction) + " Vous êtes probablement diabétique !  modèle utilisé :" + str(nom_fichier[1]))
         
     elif(selected == nom_fichier[2]):
         query = np.array([grossesses, age, insuline])
@@ -120,11 +112,7 @@ if(st.button('Predict Diabete')):
         query = query.reshape(1, 3)
         print(query)
         prediction = forest.predict(query)[0]
-        st.title("Predicted value " +
-                 str(prediction) + str(nom_fichier[2]))
-
-
-
-
-
- 
+        if prediction == 0:
+            st.title("Valeur prédite : " + str(prediction) + " Vous êtes probablement non diabétique ! modèle utilisé :" + str(nom_fichier[2]))
+        else:
+            st.title("Valeur prédite : " + str(prediction) + " Vous êtes probablement diabétique !  modèle utilisé :" + str(nom_fichier[2]))
